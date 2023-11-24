f7f193c4c425fac71893ace122c9ba0fb2bcbb5c
thuan.n <thuan.n@ycloudassethealth.onmicrosoft.com>
2023-11-24 06:24:56 +0000

    Update logic

--- models/block_2/1/block_2/generate_pickle.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from sklearn.preprocessing import MinMaxScaler
-
-data = [[0, 1], [0, 0], [1, 3], [2, 0]]
-scaler = MinMaxScaler()
-scaler.fit(data)
-with open("model.pickle", "wb") as f:
-    pickle.dump(scaler, f)

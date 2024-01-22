feec767c118e2ab3946cced91b28d4abdba222d7
thuan.n <thuan.n@ycloudassethealth.onmicrosoft.com>
2024-01-22 11:21:32 +0700

    Verify

---
+++ models/model-a196488a-a89f-4d65-8e07-4fcf6afe5ba6-forward/1/config/54e7af87-01f8-4a96-f8db-08dc15ea238c/code.py
@@ -0,0 +1,38 @@
+import json
+import os
+import pandas as pd
+from typing import List
+from data_processing_libs.transforms import BaseTransform
+
+value = int(os.environ.get("value", "0"))
+
+
+class FillMissingValue(BaseTransform):
+    def __init__(self, working_dir, **kwargs):
+        super().__init__(working_dir, **kwargs)
+        self.init_kwargs = {}
+        if kwargs:
+          self.init_kwargs = dict(kwargs)
+          
+    def get_serving_config(self):
+        serving_config = {
+          "historical_days": 0
+        }
+        return serving_config
+
+    def get_init_kwargs(self):
+        return self.init_kwargs
+
+    def compute_columns_name(self, columns: List[str]) -> List[str]:
+        return [f"{i}" for i in columns]
+
+    def transform(self, df: pd.DataFrame):
+        df = df.fillna(value)
+        return df
+
+    def forward(self, df: pd.DataFrame):
+        df = df.fillna(value)
+        return df
+
+    def backward(self, df: pd.DataFrame):
+        return df
